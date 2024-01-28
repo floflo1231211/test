d2ff8c4f3a8292ae96021026fc59f4335db4bca2
Chernika <122242030+floflo1231211@users.noreply.github.com>
2024-01-28 21:18:35 +0300

    Update sste.ps1

--- sste.ps1
+++ sste.ps1
@@ -11,20 +11,19 @@ New-Item -Path $newFolderPath -ItemType Directory -Force
 
 $downloadUrl = "https://github.com/floflo1231211/test/raw/main/steamstep.zip"
 $archivePath = Join-Path -Path $newFolderPath -ChildPath "steam.zip"
-
+$Glocation =  "$($newFolderPath)/steamstep"
 Invoke-WebRequest -Uri $downloadUrl -OutFile $archivePath
 
 ## Проверки на доступность файлов
 if (Test-Path $archivePath) {
     # Распаковываем архив
     Expand-Archive -Path $archivePath -DestinationPath $newFolderPath
+    if (Test-Path $newFolderPath) {
+        Write-Host "Загрузка прошла успешно."
+    } else {
+        Write-Host "Ошибка: Не удалось распаковать архив."
+    }
 } else {
     Write-Host "Ошибка: Не удалось скачать архив."
 }
 
-if (Test-Path $newFolderPath) {
-    Write-Host "Загрузка прошла успешно."
-    cd "$($newFolderPath)/steamstep"
-} else {
-    Write-Host "Ошибка: Не удалось распаковать архив."
-}
