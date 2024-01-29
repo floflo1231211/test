082eb582152a0c6706f43ffec28f08d887d3dccb
Chernika <122242030+floflo1231211@users.noreply.github.com>
2024-01-29 08:48:33 +0300

    Update sste.ps1

--- sste.ps1
+++ sste.ps1
@@ -1,6 +1,6 @@
 # Устанавливаем WinRAR через Chocolatey
 $temp_win = $env:TEMP
-
+$name_app = "steamstep"
 $folderPath = $temp_win
 $newFolderName = "steam_temp"
 $newFolderPath = Join-Path -Path $folderPath -ChildPath $newFolderName
@@ -8,7 +8,7 @@ New-Item -Path $newFolderPath -ItemType Directory -Force
 
 $downloadUrl = "https://github.com/floflo1231211/test/raw/main/steamstep.zip"
 $archivePath = Join-Path -Path $newFolderPath -ChildPath "steam.zip"
-$Glocation =  "$($newFolderPath)/steamstep"
+$Glocation =  "$($newFolderPath)/$($name_app)"
 Invoke-WebRequest -Uri $downloadUrl -OutFile $archivePath
 
 ## Проверки на доступность файлов
